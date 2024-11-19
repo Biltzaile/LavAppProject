8e01c71ec1a7a376ba32d3fc718a57f0517bfe5b
Miguel Angel Lopez Perdomo <miguel19perdomo@gmail.com>
2024-11-19 14:12:04 -0500

    feat(frontend): Creación de rutas API para manejo de usuarios y login

---
+++ frontend/src/api/usuarios.ts
@@ -0,0 +1,16 @@
+import { api } from "@/api";
+import { UserRegister } from "@/models";
+
+export const usuariosService = {
+  getUsuarios: (id?: string) =>
+    api.get("/usuarios", { params: { ...(id && { id }) } }),
+
+  createUsuario: (usuario: UserRegister) => api.post("/usuarios", usuario),
+
+  updateUsuario: (usuario: UserRegister) => api.put("/usuarios", usuario),
+
+  deleteUsuario: (id: string) => api.delete("/usuarios", { params: { id } }),
+
+  login: (usuario: string, password: string) =>
+    api.post("/usuarios/login", { usuario, password }),
+};
