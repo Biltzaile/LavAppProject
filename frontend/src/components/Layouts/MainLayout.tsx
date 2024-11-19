51738722b6b4da22fb03e911ff1401f491aa450c
Miguel Angel Lopez Perdomo <miguel19perdomo@gmail.com>
2024-11-19 14:10:41 -0500

    feat(frontend): Añadir Componente Toaster
    Para mostrar mensajes al usuario: error, advertencia, éxito, info.

--- frontend/src/components/Layouts/MainLayout.tsx
+++ frontend/src/components/Layouts/MainLayout.tsx
@@ -1,5 +1,5 @@
 import { Outlet, useLocation } from "react-router-dom"
-import { Avatar, AvatarFallback, AvatarImage, DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "../ui"
+import { Avatar, AvatarFallback, AvatarImage, DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger, Toaster } from "../ui"
 import { useAppStore, useAuthStore } from "@/contexts"
 
 export const MainLayout = () => {
@@ -24,12 +24,6 @@ export const MainLayout = () => {
   return (
     <div className="flex flex-col h-screen">
       <header className="flex justify-between items-center bg-primary text-primary-foreground p-4">
-        {/* 
-          - logo or company name from appStore
-          - Title of the page
-            - Dashboard (if on /dashboard) + role name from authStore
-          - Avatar + submenu with logout button
-        */}
         <div className="flex items-center">
           {
             company?.logo ?
@@ -58,6 +52,7 @@ export const MainLayout = () => {
       <main className="flex justify-center items-center h-full p-4">
         <Outlet />
       </main>
+      <Toaster richColors theme="light" />
     </div>
   )
 }
