107eef3e0c6c10b049eb50043cf47a3d4e9405de
Miguel Angel Lopez Perdomo <miguel19perdomo@gmail.com>
2024-11-23 04:02:32 -0500

    feat(frontend): Breadcrumb en MainLayout

--- frontend/src/components/Layouts/MainLayout.tsx
+++ frontend/src/components/Layouts/MainLayout.tsx
@@ -1,9 +1,10 @@
-import { Outlet, useLocation } from "react-router-dom"
+import { Outlet, useLocation, useNavigate } from "react-router-dom"
 import { Avatar, AvatarFallback, AvatarImage, DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger, Toaster } from "../ui"
 import { useAppStore, useAuthStore } from "@/contexts"
+import { Home } from "lucide-react"
 
 export const MainLayout = () => {
-
+  const navigate = useNavigate()
   const { empresa: company } = useAppStore()
 
   const { user, logout } = useAuthStore()
@@ -14,7 +15,19 @@ export const MainLayout = () => {
     if (path === 'dashboard') {
       return `Dashboard ${user?.rol ? `- ${user.rol}` : ''}`
     }
-    return path.charAt(0).toUpperCase() + path.slice(1) // Capitalize first letter
+    return (
+      <div className="flex items-center gap-2">
+        <span
+          className="cursor-pointer hover:opacity-80 flex items-center gap-1"
+          onClick={() => navigate('/dashboard')}
+        >
+          <Home size={"1.5rem"} strokeWidth={3} />
+          Dashboard
+        </span>
+        <span>{'>'}</span>
+        <span>{path.charAt(0).toUpperCase() + path.slice(1)}</span>
+      </div>
+    )
   }
 
   const handleLogout = () => {
@@ -23,8 +36,8 @@ export const MainLayout = () => {
 
   return (
     <div className="flex flex-col h-screen">
-      <header className="flex justify-between items-center bg-primary text-primary-foreground p-4">
-        <div className="flex items-center">
+      <header className="flex justify-between items-center bg-primary text-primary-foreground p-4 select-none">
+        <div className="flex items-center cursor-pointer" onClick={() => navigate('/dashboard')}>
           {
             company?.logo ?
               <img src={company?.logo} alt={company?.nombre} className="h-8 w-auto" />
@@ -49,7 +62,7 @@ export const MainLayout = () => {
           </DropdownMenuContent>
         </DropdownMenu>
       </header>
-      <main className="flex justify-center items-center h-full p-4">
+      <main className="flex justify-center items-center h-full p-4 select-none">
         <Outlet />
       </main>
       <Toaster richColors theme="light" />
